a9239028561d6a39a4e89d708661eba1b4476863
BenXHub <90320051+BenXHub@users.noreply.github.com>
2023-10-13 16:25:57 +0800

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -12,17 +12,48 @@
     <div class="container mt-3">
         <div class="row">
             <div class="col-md-8 offset-md-2">
+                @if(session('success'))
+                    <div class="alert alert-danger" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if(session('error'))
+                    <div class="alert alert-danger" role="alert">
+                        {{ session('error') }}
+                    </div>
+                @endif
                 <div class="d-flex justify-content-between align-items-center mb-3">
-                    <h2>Students</h2>
+                    <h2 class="text-success">Student List</h2>
                     <a href="{{ route('student_create') }}" class="btn btn-success">Add Student</a>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit nobis non quos repellendus sint?</p>
+                <h5 class="mb-4">Curiosity Unleashed, Potential Unlocked!.</h5>
 
                 <!-- Student List -->
-                <div class="list-group">
-                    @foreach($students as $key => $student)
-                        <a class="list-group-item" href="{{ route('student_show', ['id' => $student['id']]) }}">{{ $student['first_name'] }} {{ $student['last_name'] }}</a>
-                    @endforeach
+                <div>
+                    <table class="table table-hover table-student-info ">
+                        <thead class="table-success">
+                            <tr>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($students as $key => $student)
+                                <tr>
+                                    <td>
+                                        <a class="list-group-item" style="font-weight: normal; color: inherit">{{ $student['first_name'] }}</a>
+                                    </td>
+                                    <td>
+                                        <a class="list-group-item" style="color: inherit">{{ $student['last_name'] }}</a>
+                                    </td>
+                                    <td>
+                                        <a class="list-group-item" href="{{ route('student_show', ['id' => $student['id']]) }}"><button class="text-bg-light" style="border-radius: 0 32px 0 32px; border: 1px solid black">👁️</button></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
